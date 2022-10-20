984f0d5e6d6cbd25e34a07ca9248a8c7bc022908
donsmith30 <donsmith30@hotmail.com>
2022-10-20 13:36:15 -0700

    Modified quit confirmations, AB#13

--- src/main/java/com/caged/GameControl.java
+++ src/main/java/com/caged/GameControl.java
@@ -1,68 +1,70 @@
 package com.caged;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-
-import java.io.Console;
-import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class GameControl {
 
     private boolean userInput = false;
-    private Scanner scanner = new Scanner(System.in);
-    private boolean playGame = true;
+    private final Scanner scanner = new Scanner(System.in);
+    private final boolean playGame = true;
     Scanner in = new Scanner(System.in);
     TextParser textParser = new TextParser();
     YAMLReader yamlReader = new YAMLReader();
-    //Player player = new Player();
+    SplashScreen splashScreen = new SplashScreen();
+    MainMenu mainMenu = new MainMenu();
 
     public void runGame() {
 //        Console.clear();
 //         research clear method
-//        SplashScreen.show();
-//        Intro.show();
-//        Intro.startOption();
-//        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-//        try {
-//            Player player =  mapper.readValue(new File("./resources/player.yml"), Player.class);
-//            System.out.println("New player: " + player.getName());
-//            playGame(player);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-        yamlReader.introLoader();
+        splashScreen.splash();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        mainMenu.mainMenu();
+        mainMenuOptions();
         playGame(yamlReader.playerLoader());
     }
 
-    private void playGame(Player player){
-        while (playGame){
-            System.out.println("\n>>>>");
+    private void playGame(Player player) {
+        while (playGame) {
+            System.out.print(">>>>");
             String userChoice = in.nextLine();
             String lowUser = userChoice.toLowerCase();
             if (lowUser.equals("quit")) {
-                playGame = false;
-            }
-            else {
+                quitConfirm();
+            } else {
                 String[] action = textParser.textParser(lowUser);
-                player.playerActions(action[0],action[1]);
+                player.playerActions(action[0], action[1]);
             }
         }
     }
 
-    public void newGame() {
+    public void mainMenuOptions() {
         while (!userInput) {
             System.out.print("\n>>>> ");
             String input = scanner.nextLine().toLowerCase();
             if (input.equals("new game")) {
-                System.out.println(" #### You are CAGED #### ");    //Game starts - starting with Game Intro
-
-            } else {
-                System.out.println("Invalid input, please enter valid input");
+                yamlReader.introLoader();
                 userInput = true;
+            } else if (input.equals("quit")) {
+                quitConfirm();
             }
+            else {
+                System.out.println("Invalid input, please enter valid input");
+            }
+        }
+    }
+
+    public void quitConfirm(){
+        System.out.println("Do you really want to quit?");
+        String confirm = scanner.nextLine().toLowerCase();
+        if (confirm.equals("yes")){
+            System.exit(0);
+        }
+        else {
+            System.out.println("Didn't say yes...Still caged...");
         }
     }
 
