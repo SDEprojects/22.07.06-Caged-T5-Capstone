3123a6111c25c86e3fe91741a64741c1a53de5c2
donsmith30 <donsmith30@hotmail.com>
2022-10-20 12:42:38 -0700

    Completed yamlReader class, AB#22

--- src/main/java/com/caged/YAMLReader.java
+++ src/main/java/com/caged/YAMLReader.java
@@ -5,14 +5,15 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInput;
 
 class YAMLReader {
+
     ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
     public Player playerLoader(){
         try {
             Player player =  mapper.readValue(new File("./resources/player.yml"), Player.class);
-            System.out.println("New player: " + player.getName());
             return player;
         } catch (
                 IOException e) {
@@ -21,14 +22,57 @@ class YAMLReader {
         return null;
     }
 
-    public String introLoader() {
-        String result = "";
+    public DictionaryGetter dictionaryLoader(){
+        try {
+            DictionaryGetter dictionary =  mapper.readValue(new File("./resources/dictionary.yml"), DictionaryGetter.class);
+            return dictionary;
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public CharacterNPCGetter characterNPCLoader(){
+        try {
+            CharacterNPCGetter CharacterNPC =  mapper.readValue(new File("./resources/CharacterNPC.yml"), CharacterNPCGetter.class);
+            return CharacterNPC;
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ItemGetter itemLoader(){
+        try {
+            ItemGetter item =  mapper.readValue(new File("./resources/Items.yml"), ItemGetter.class);
+            return item;
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public LocationGetter locationLoader(){
+        try {
+            LocationGetter location =  mapper.readValue(new File("./resources/Location.yml"), LocationGetter.class);
+            return location;
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void introLoader() {
         try {
             Information info = mapper.readValue(new File("./resources/Game_info.yml"), Information.class);
-            result = info.getStory().get(0);
+            String result = info.getStory().get(0);
+            System.out.println(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return result;
     }
 }
