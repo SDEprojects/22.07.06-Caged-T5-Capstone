1e80f956a17632f1fa449e474d348beead7befc6
Gaurav Banjade <helohsm78@gmail.com>
2022-11-03 09:29:51 -0700

    GUI, AB#732

--- src/main/java/com/caged/client/Main.java
+++ src/main/java/com/caged/client/Main.java
@@ -2,16 +2,18 @@ package com.caged.client;
 
 import com.caged.GameControl;
 import com.caged.gui.MainWindow;
+import com.caged.gui.PlayWindow;
 
 
 class Main {
 
     public static void main(String[] args) {
-//        GameControl newGame = new GameControl();
-//        newGame.runGame();
-
         MainWindow main = new MainWindow();
         main.execute();
+
+//        PlayWindow play = new PlayWindow();
+//        play.execute();
+
 
 
     }
